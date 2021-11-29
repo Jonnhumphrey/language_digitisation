6ff3123b65b6558a4af5bd17c1f0aa88a0346a4f
Jonnhumphrey <94903620+Jonnhumphrey@users.noreply.github.com>
2021-11-29 13:06:33 +0000

    Update google_updater.py

--- lang_dig_scripts/scripts/google_updater.py
+++ lang_dig_scripts/scripts/google_updater.py
@@ -7,9 +7,11 @@ def init_google_sheet():
                                           'language-digitization-project-5c87586d60ad.json')
     return gc
 
+
 def open_google_sheet(gc, sheetname):
     sheet = gc.open(sheetname)
     return sheet
+
 
 def set_google_sheet(sheet, sheet_name, df):
     print("Setting sheet for : ", sheet_name)
@@ -18,11 +20,19 @@ def set_google_sheet(sheet, sheet_name, df):
     wks.set_dataframe(df, (1, 1))
     print("Sheet set for : ", sheet_name)
 
+    
+def get_sheet():
+    gc = init_google_sheet()
+    g_sheet = open_google_sheet(gc, "Language Digitization Resources")
+    return g_sheet
+    
+    
 def update_cell(sheet, sheet_name):
     wks = sheet.worksheet_by_title(sheet_name)
     wks.update_value('B1', datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S"))
     print("Timestamp updated")
 
+    
 def update_google_sheets(sheet, list_of_dfs):
     for site in list_of_dfs:
         set_google_sheet(sheet, list_of_dfs[site].get("filename"), list_of_dfs[site].get("df"))
